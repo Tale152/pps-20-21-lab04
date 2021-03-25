483438001861d43df0d4285760f1a213d08acdf9
tale97 <alessandro.talmi@studio.unibo.it>
2021-03-25 18:29:57 +0100

    SameTeacherTest refactor

--- test/u04lab/code/SameTeacherTest.scala
+++ test/u04lab/code/SameTeacherTest.scala
@@ -2,15 +2,17 @@ package u04lab.code
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import u04lab.code.Lists.List.{Cons, Nil}
+import u04lab.code.Lists.List.Nil
 
 class SameTeacherTest {
 
   @Test
   def testSameTeacher(): Unit ={
 
-    val listSameTeacher = Cons(Course("PPS","Viroli"), Cons(Course("OOP", "Viroli"), Nil()))
-    val listDifferentTeachers = Cons(Course("PPS","Viroli"), Cons(Course("PCD","Ricci"), Nil()))
+    val listFactory = new ListFactoryImpl()
+
+    val listSameTeacher = listFactory.fromVariadicArgs(Course("PPS","Viroli"), Course("OOP", "Viroli"))
+    val listDifferentTeachers = listFactory.fromVariadicArgs(Course("PPS","Viroli"), Course("PCD","Ricci"))
     val listEmpty = Nil[Course]()
 
     assertEquals(Some("Viroli"), SameTeacher.unapply(listSameTeacher))
